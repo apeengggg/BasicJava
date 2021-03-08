996ca7255a0378e5167b5bd2053fa76227079746
apeengggg <mirfanmanaf0804@gmail.com>
2021-03-08 09:49:16 +0700

    perulangan for

---
+++ src/perulangan/Perulangan.java
@@ -0,0 +1,19 @@
+package perulangan;
+
+public class Perulangan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		inisiasi variabe a = 0
+		int a=0;        
+//		a=0, kondisi awal
+//		a<5, kondisi jika a<5 lakukan perulangan tersebut
+//		a++, lakukan perulangan jika a<5
+		for(a=0;a<5;a++){
+//			print / cetak nilai a
+			System.out.println("Nilai a :" +a);
+		}
+
+	}
+
+}
