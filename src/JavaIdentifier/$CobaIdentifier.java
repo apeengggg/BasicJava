c2852bc1db62e2384c87c2c2368b8ca9fb7586ff
apeengggg <mirfanmanaf0804@gmail.com>
2021-03-08 09:28:11 +0700

    identifier

---
+++ src/JavaIdentifier/$CobaIdentifier.java
@@ -0,0 +1,16 @@
+package JavaIdentifier;
+
+public class $CobaIdentifier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String $nama, tanggal_lahir, deskripsi;
+		$nama = "James Arthur Gosling";
+		tanggal_lahir = "born May 19, 1955";
+		deskripsi = "The father of Java Programming Language";
+		System.out.println($nama);
+		System.out.println(tanggal_lahir);
+		System.out.println(deskripsi);
+	}	
+
+}
