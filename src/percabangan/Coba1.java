33521a4ba45008e2ceb9e493f185a29e2a8b98d9
apeengggg <mirfanmanaf0804@gmail.com>
2021-03-08 09:38:35 +0700

    percabangan if else

---
+++ src/percabangan/Coba1.java
@@ -0,0 +1,21 @@
+package percabangan;
+
+public class Coba1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		inisiasi varibale a = 1;
+		int a=1;        
+//		kondisi 1
+		if (a==0)
+//			statemen jika kondisi 1 true
+		   			System.out.println("Nilai a = 0");
+//		kondisi 2
+		else
+//			statemen jika kondisi 1 False
+		   			System.out.println("Nilai a = 1");
+		
+//		outputnya adalah "Nilai a = 1" karena nilai a != 0  (sesuai dengan kondisi 1) / FALSE
+	}
+
+}
