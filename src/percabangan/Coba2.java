56f1e08ac9a14dfac5390506cb70fac9d398dd0a
apeengggg <mirfanmanaf0804@gmail.com>
2021-03-08 09:43:38 +0700

    percabangan if elseif

--- src/percabangan/Coba2.java
+++ src/percabangan/Coba2.java
@@ -14,6 +14,10 @@ public class Coba2 {
 		else if(a==1)
 //			statemen jika kondisi 2 True
 		   			System.out.println("Nilai a = 1");
+//		jika kondisi 1 dan 2, cek dengan kondisi 3 dibawah ini
+		else if (a==2)
+//			statement jika kondisi 3 true
+					System.out.println("Nilai a = 2");
 		
 //		outputnya adalah "Nilai a = 1" karena nilai a == 1  (sesuai dengan kondisi 2) / TRUE
 	}
