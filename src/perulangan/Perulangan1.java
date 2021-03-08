dd6451462b1d2278b49e7ac682d6833b9b85b6b5
apeengggg <mirfanmanaf0804@gmail.com>
2021-03-08 09:50:44 +0700

    perulangan while

---
+++ src/perulangan/Perulangan1.java
@@ -0,0 +1,19 @@
+package perulangan;
+
+public class Perulangan1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		inisiasi variabel a =0 dengan tipe data integer (int)
+		int a=0;   
+//		kondisi jika <5 lakukan statement perulangan
+		while(a<5){
+//			statement
+			System.out.println("Nilai a : "+a);
+//			perulangan
+			a++;
+		}
+
+	}
+
+}
