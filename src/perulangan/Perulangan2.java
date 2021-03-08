207c09c49696d9533675f109b5a7b69d0cf0cfae
apeengggg <mirfanmanaf0804@gmail.com>
2021-03-08 09:52:18 +0700

    perulangan do while

---
+++ src/perulangan/Perulangan2.java
@@ -0,0 +1,20 @@
+package perulangan;
+
+public class Perulangan2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		inisiasi variabel a = 0 dengan tipe data integer (int)
+		int a=0;        
+//		lakukan cetak statemen
+		do{
+//			statement
+			System.out.println("Nilai a : "+a);
+//			perulangan
+			a++;
+//			kondisi jika a<5 lakukan perulangan
+		}while(a<5);
+
+	}
+
+}
