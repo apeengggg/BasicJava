b8f6386d1ddcc118c593da01493a4bb8fe204664
apeengggg <mirfanmanaf0804@gmail.com>
2021-03-08 09:33:56 +0700

    keyword final

---
+++ src/keywordFinal/FinalKeyword.java
@@ -0,0 +1,13 @@
+package keywordFinal;
+
+public class FinalKeyword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		inisisi variabel a dengan keyword final (tidak dapat diubah)
+		final int a = 5;
+//		eror karena ada keyword final pada inisiasi variabel a
+//		a = 10;
+	}	
+
+}
